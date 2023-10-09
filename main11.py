3a659a7b7ff5602ddce16f74bca07d6681990293
amiliyaa <99253568+amiliyaa@users.noreply.github.com>
2023-10-09 13:35:44 +0600

    1st part of the second project

---
+++ main11.py
@@ -0,0 +1,12 @@
+f = open ("test.txt", 'r')
+print("readline() method")
+print(f.readline ())
+print(f.readline ())
+f. close ()
+
+f = open ("test.txt", 'r')
+print ("readlines () method")
+lines = f.readlines ()
+for line in lines:
+   print(line.strip())
+f.close ()
